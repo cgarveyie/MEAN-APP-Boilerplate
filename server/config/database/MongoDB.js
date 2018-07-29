754531fad23b02e32fad9a08ccf22a97d0622420
DaniMezhi <mezhidani@gmail.com>
2018-07-29 21:19:20 +0300

    sync

---
+++ server/config/database/MongoDB.js
@@ -0,0 +1,45 @@
+import ModelFactory from '../factories/ModelFactory';
+
+const moogoose = require('mongoose');
+
+export default class MongoDB {
+
+    constructor() {
+        this.host = ''
+        this.user = ''
+        this.password = ''
+
+        //Connect to db
+        let url = `mongodb+srv://${this.user}:${this.password}@${this.host}`;
+        moogoose.connect(url, { useNewUrlParser: true }, err => {
+            if (err) {
+                console.log('Error: ' + err);
+            } else {
+                console.log('Connected to MongoDB');
+            }
+        });
+    }
+
+    insert(to, data) {
+        //Get model instance 
+        let model = new ModelFactory(to);
+
+        if (!model) {
+            throw new Error(`Error occured, model not exist`)
+        }
+
+        let schema = model.getSchema();
+
+        try {
+            schema.save((err, _res) => {
+                if (err) {
+                    throw new Error(`Error occured, can not insert data to ${model.getName()}`)
+                }
+
+                return _res;
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
