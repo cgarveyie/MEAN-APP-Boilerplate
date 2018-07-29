aa7c830df14a521c4801548b565de0e32f9d1a91
DaniMezhi <mezhidani@gmail.com>
2018-07-29 20:20:07 +0300

    sync

--- server/config/database/MongoDB.js
+++ server/config/database/MongoDB.js
@@ -5,9 +5,9 @@ const moogoose = require('mongoose');
 export default class MongoDB {
 
     constructor() {
-        this.host = 'alphadev-xeuzq.mongodb.net/test?retryWrites=true'
-        this.user = 'DaMezhi'
-        this.password = 'kamagistdani16'
+        this.host = ''
+        this.user = ''
+        this.password = ''
 
         //Connect to db
         let url = `mongodb+srv://${this.user}:${this.password}@${this.host}`;
